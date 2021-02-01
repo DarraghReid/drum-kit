7e84a6e10343e88e308a630a5a6b5a85a032f2fd
DarraghReid <darragh.reid@gmail.com>
2021-02-01 20:49:32 +0000

    itial commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+function playSound(event) {
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition() {
+    this.classList.remove('playing');
+};
+
+let keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
